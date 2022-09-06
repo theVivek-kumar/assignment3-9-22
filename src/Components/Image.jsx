b0a6ef45819f4dd9f4b70f21201b8ca57a2d4d62
Vivek kumar <57774129+theVivek-kumar@users.noreply.github.com>
2022-09-07 00:58:06 +0530

    add navbar and model and card

---
+++ src/Components/Image.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Image() {
+  return (
+    <div>
+    <img class="max-w-full h-auto" src="/Images/thumb.jpg" alt="image description"/>
+    </div>
+  )
+}
+
+export default Image
