02b7d0d231caa1992d7c9858ed289e25adcea06c
Vivek kumar <57774129+theVivek-kumar@users.noreply.github.com>
2022-09-07 03:15:28 +0530

    add-componennt

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/navbar';
 import Image from './Components/Image';
+import Card from './Components/Card';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <Image/>
+    <div >
+      <Card/>
+    </div>
 
     </div>
   );
